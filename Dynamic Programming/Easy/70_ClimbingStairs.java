1ca4f96d960b13a2a33638fa367dbaff1da19971
pranesh k <145831392+Pranesh003@users.noreply.github.com>
2026-03-26 09:59:47 +0530

    Create 70_ClimbingStairs.java

---
+++ Dynamic Programming/Easy/70_ClimbingStairs.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int climbStairs(int n) {
+        int pre1=1;
+        int pre2=1;
+
+        for(int i=2;i<=n;i++){
+            int curr= pre1+pre2;
+            pre2=pre1;
+            pre1=curr;
+
+        }
+        return pre1;
+    }
+}
