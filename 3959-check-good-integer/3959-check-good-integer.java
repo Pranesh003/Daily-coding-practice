028a7f4d1ede839b7dacb786a4d2cbcee7569f88
pranesh k <145831392+Pranesh003@users.noreply.github.com>
2026-06-15 21:49:25 +0530

    Time: 1 ms (100%), Space: 42.4 MB (0%) - LeetHub

--- 3959-check-good-integer/3959-check-good-integer.java
+++ 3959-check-good-integer/3959-check-good-integer.java
@@ -1,24 +1,14 @@
 class Solution {
     public boolean checkGoodInteger(int n) {
-        int[] digits = String.valueOf(n)
-        .chars()
-        .map(c -> c - '0')
-        .toArray();
-        int totalSum=0;
-        int squareSum=0;
-        for(int i=0;i<digits.length;i++){
-            totalSum+=digits[i];
+        int a = 0, b = 0;
 
-            squareSum+=digits[i]*digits[i];
-
-            
+        while (n > 0) {
+            int temp = n % 10;
+            a += temp;
+            b += temp * temp;
+            n /= 10;
         }
 
-        int diff=squareSum-totalSum;
-        if(diff>=50){
-            return true;
-        }
-
-        return false;
+        return b - a >= 50;
     }
 }
