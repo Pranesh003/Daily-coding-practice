08f828c7761a8c3f5ed34fdecce3529aa86eb4b0
pranesh k <145831392+Pranesh003@users.noreply.github.com>
2026-07-15 11:45:11 +0530

    Time: 2 ms (92.18%), Space: 47.7 MB (57.13%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,24 +1,17 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int n=nums.length;
-
-        int[] temp=new int[n];
         int j=0;
-
-        for(int i=0;i<n;i++){
+        for(int i=0;i<nums.length;i++){
             if(nums[i]!=0){
-            temp[j++]=nums[i];
+                int temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+
+            j++;
             }
-        }
 
-        while(j<n){
-            temp[j++]=0;
+            
         }
-
-        for(int i=0;i<n;i++){
-            nums[i]=temp[i];
-        }
-        }
-
-  
+       
     }
+}
