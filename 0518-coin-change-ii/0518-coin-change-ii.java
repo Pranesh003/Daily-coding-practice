36efaad572b80302b3ed07973580f0216f8f3b02
pranesh k <145831392+Pranesh003@users.noreply.github.com>
2026-07-27 22:14:36 +0530

    Time: 13 ms (43.03%), Space: 56.9 MB (38.4%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,35 +1,29 @@
 class Solution {
 
-    int[][] dp;
-
-    public int solve(int index, int[] coins, int amount) {
-
-        if (amount == 0)
-            return 1;
-
-        if (index == coins.length)
-            return 0;
-
-        if (dp[index][amount] != -1)
-            return dp[index][amount];
-
-        int notTake = solve(index + 1, coins, amount);
-
-        int take = 0;
-
-        if (coins[index] <= amount)
-            take = solve(index, coins, amount - coins[index]);
-
-        return dp[index][amount] = take + notTake;
-    }
-
     public int change(int amount, int[] coins) {
 
-        dp = new int[coins.length][amount + 1];
+        int n = coins.length;
 
-        for (int i = 0; i < coins.length; i++)
-            Arrays.fill(dp[i], -1);
+        int[][] dp = new int[n + 1][amount + 1];
 
-        return solve(0, coins, amount);
+        // Base case
+        dp[n][0] = 1;
+
+        for (int index = n - 1; index >= 0; index--) {
+
+            for (int amt = 0; amt <= amount; amt++) {
+
+                int notTake = dp[index + 1][amt];
+
+                int take = 0;
+
+                if (coins[index] <= amt)
+                    take = dp[index][amt - coins[index]];
+
+                dp[index][amt] = take + notTake;
+            }
+        }
+
+        return dp[0][amount];
     }
 }
