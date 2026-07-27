3d92b09f13ab2044f21fad18878c875976ff963b
pranesh k <145831392+Pranesh003@users.noreply.github.com>
2026-07-27 21:57:32 +0530

    Time: 29 ms (16.65%), Space: 57.1 MB (29.04%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -0,0 +1,35 @@
+class Solution {
+
+    int[][] dp;
+
+    public int solve(int index, int[] coins, int amount) {
+
+        if (amount == 0)
+            return 1;
+
+        if (index == coins.length)
+            return 0;
+
+        if (dp[index][amount] != -1)
+            return dp[index][amount];
+
+        int notTake = solve(index + 1, coins, amount);
+
+        int take = 0;
+
+        if (coins[index] <= amount)
+            take = solve(index, coins, amount - coins[index]);
+
+        return dp[index][amount] = take + notTake;
+    }
+
+    public int change(int amount, int[] coins) {
+
+        dp = new int[coins.length][amount + 1];
+
+        for (int i = 0; i < coins.length; i++)
+            Arrays.fill(dp[i], -1);
+
+        return solve(0, coins, amount);
+    }
+}
